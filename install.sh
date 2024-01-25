cd9e4ef620127ec23e4dfbd0bf3903a9676d5653
Joan Francesc Pedro Garcia <joan.f.pedro@protonmail.com>
2024-01-25 18:30:11 +1100

    Setup wizards deleted

--- install.sh
+++ install.sh
@@ -2,9 +2,25 @@
 
 # Installation of the environment.
 
-function questionYN () {
+function QuestionYN () {
     read -p "$1? (Y/N): " response
     [ -z "$response" ] || [ "$response" = "y" ] || [ "$response" = "Y" ]
+}
+
+function DockerCompose () {
+    cd $1
+    docker compose up -d
+    cd ..
+}
+
+function SetupGitea () {
+    # run ansible script
+    echo ""
+}
+
+function SetupJenkins () {
+    # run ansible script
+    echo ""
 }
 
 echo ''
@@ -13,14 +29,12 @@ echo ''
 echo 'Make sure you have the following requisites installed before proceding: '
 echo '- Docker and docker compose (https://docs.docker.com/engine/install/)'
 echo '- Ansible (https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html)'
+echo ''
 
-if questionYN "question"; then
-    echo "ok"
+if QuestionYN "Do you want to run the DevOps environment"; then
+    DockerCompose "DevOps-env"
 fi
 
-## Set up docker environments
-
-
-## Automatize all systems
-
-## Create pipelines
+if QuestionYN "Do you want to run the testing environment"; then
+    DockerCompose "DevOps-env"
+fi
